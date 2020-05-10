bb6b12fae1c7140044b2f772e18533b5e9912192
harikrishnan-a-k <harikrishnan.a.k.c@gmail.com>
2020-05-10 15:36:48 +0530

    Updates

---
+++ precache-manifest.e9afb598480e03f574147d55667145d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ff46108cc83d97b90f1fb13fb2a8c4",
+    "url": "/Shopping-Cart-demo-with-React/index.html"
+  },
+  {
+    "revision": "6908a95842e2f6df2886",
+    "url": "/Shopping-Cart-demo-with-React/static/css/main.cf810265.chunk.css"
+  },
+  {
+    "revision": "b8988b0c34367addd624",
+    "url": "/Shopping-Cart-demo-with-React/static/js/2.16c1f389.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Shopping-Cart-demo-with-React/static/js/2.16c1f389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6908a95842e2f6df2886",
+    "url": "/Shopping-Cart-demo-with-React/static/js/main.62abf938.chunk.js"
+  },
+  {
+    "revision": "8b646ce4c562969da7cc",
+    "url": "/Shopping-Cart-demo-with-React/static/js/runtime-main.d83683ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Shopping-Cart-demo-with-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
